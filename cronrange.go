4bb77ca380bb9dcc044d488567999e66846e0d1e
Angel Analoosen <an9an63@gmail.com>
2019-11-10 12:54:53 +0800

    Implement New with test

--- cronrange.go
+++ cronrange.go
@@ -3,21 +3,23 @@ package cronrange
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
 var (
-	cronZeroTime = time.Time{}
+	cronZeroTime    = time.Time{}
 	cronParseOption = cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow
 	cronParser      = cron.NewParser(cronParseOption)
 )
 
 type CronRange struct {
-	Expression  string
-	TimeZone    string
-	DurationMin uint64
+	cronExpression string
+	timeZone       string
+	duration       time.Duration
+	schedule       cron.Schedule
 }
 
 type TimeRange struct {
@@ -25,54 +27,29 @@ type TimeRange struct {
 	End   time.Time
 }
 
-func New(expression, timeZone string, durationMin uint64) *CronRange {
-	return &CronRange{
-		Expression:  expression,
-		TimeZone:    timeZone,
-		DurationMin: durationMin,
-	}
-}
-
-func (cr *CronRange) NextOccurrences(t time.Time, count int) (occurs []TimeRange, err error) {
-	if cr.DurationMin <= 0 {
+func New(cronExpr, timeZone string, durationMin uint64) (cr *CronRange, err error) {
+	if durationMin <= 0 {
 		err = errors.New("duration should be positive")
 		return
 	}
 
-	cronSpec := cr.Expression
-	if len(cr.TimeZone) > 0 {
-		cronSpec = fmt.Sprintf("CRON_TZ=%s %s", cr.TimeZone, cr.Expression)
+	cronSpec := cronExpr
+	if strings.ToLower(strings.TrimSpace(timeZone)) == "local" {
+		timeZone = ""
+	} else if len(timeZone) > 0 {
+		cronSpec = fmt.Sprintf("CRON_TZ=%s %s", timeZone, cronExpr)
 	}
 
-	var sched cron.Schedule
-	if sched, err = cronParser.Parse(cronSpec); err != nil {
+	var schedule cron.Schedule
+	if schedule, err = cronParser.Parse(cronSpec); err != nil {
 		return
 	}
 
-	duration := time.Minute * time.Duration(cr.DurationMin)
-	for curr, i := t, 0; i < count; i++ {
-		next := sched.Next(curr)
-		if next == cronZeroTime {
-			break
-		}
-		occur := TimeRange{
-			Start: next,
-			End:   next.Add(duration),
-		}
-		fmt.Println(occur)
-		occurs = append(occurs, occur)
-		curr = next
-	}
-
-	return
-}
-
-func NextOccur(spec string) (t time.Time, err error) {
-	// spec := "CRON_TZ=Asia/Tokyo 8 * * * 6"
-	specParser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
-	sched, err := specParser.Parse(spec)
-	if err == nil {
-		t = sched.Next(time.Now())
+	cr = &CronRange{
+		cronExpression: cronExpr,
+		timeZone:       timeZone,
+		duration:       time.Minute * time.Duration(durationMin),
+		schedule:       schedule,
 	}
 	return
 }
--- cronrange_test.go
+++ cronrange_test.go
@@ -1,68 +1,40 @@
 package cronrange
 
 import (
-	"reflect"
 	"testing"
-	"time"
 )
 
-func TestNextOccur(t *testing.T) {
+func TestNew(t *testing.T) {
+	type args struct {
+		cronExpr    string
+		timeZone    string
+		durationMin uint64
+	}
 	tests := []struct {
 		name    string
-		spec    string
-		wantT   time.Time
+		args    args
+		wantCr  bool
 		wantErr bool
 	}{
-		{"Bangkok Monday Morning", "CRON_TZ=Asia/Bangkok 0 7 * * 1", time.Date(2019, 11, 11, 0, 0, 0, 0, time.UTC), false},
-		{"Tokyo Monday Morning", "CRON_TZ=Asia/Tokyo 0 9 * * 1", time.Date(2019, 11, 11, 0, 0, 0, 0, time.UTC), false},
+		{"Empty cronExpr", args{"", "", 5}, false, true},
+		{"Invalid cronExpr", args{"h e l l o", "", 5}, false, true},
+		{"Incomplete cronExpr", args{"* * * *", "", 5}, false, true},
+		{"Nonexistent timezone", args{"* * * * *", "Mars", 5}, false, true},
+		{"Zero durationMin", args{"* * * * *", "", 0}, false, true},
+		{"Normal without timezone", args{"* * * * *", "", 5}, true, false},
+		{"Normal with local timezone", args{"* * * * *", " Local ", 5}, true, false},
+		{"Normal with timezone", args{"* * * * *", "Asia/Bangkok", 5}, true, false},
+		{"Normal with large duration", args{"* * * * *", "Asia/Bangkok", 5259000}, true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotT, err := NextOccur(tt.spec)
+			gotCr, err := New(tt.args.cronExpr, tt.args.timeZone, tt.args.durationMin)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("NextOccur() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if gotT.UTC() != tt.wantT.UTC() {
-				t.Errorf("NextOccur() gotT = %v, want %v", gotT, tt.wantT)
-			}
-		})
-	}
-}
-
-func TestCronRange_NextOccurrences(t *testing.T) {
-	type fields struct {
-		Expression  string
-		TimeZone    string
-		DurationMin uint64
-	}
-	type args struct {
-		t     time.Time
-		count int
-	}
-	tests := []struct {
-		name       string
-		fields     fields
-		args       args
-		wantOccurs []TimeRange
-		wantErr    bool
-	}{
-		{"First 5 minutes in every hour", fields{"0 * * * *", "", 5}, args{time.Now(), 5}, nil, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cr := &CronRange{
-				Expression:  tt.fields.Expression,
-				TimeZone:    tt.fields.TimeZone,
-				DurationMin: tt.fields.DurationMin,
-			}
-			gotOccurs, err := cr.NextOccurrences(tt.args.t, tt.args.count)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NextOccurrences() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotOccurs, tt.wantOccurs) {
-				t.Errorf("NextOccurrences() gotOccurs = %v, want %v", gotOccurs, tt.wantOccurs)
+			if (gotCr != nil) != tt.wantCr {
+				t.Errorf("New() gotCr = %v, wantCr %v", gotCr, tt.wantCr)
 			}
 		})
 	}
