1105268d4a80584241610cec09accecc5f3bcd6b
Angel Analoosen <an9an63@gmail.com>
2019-11-10 20:11:55 +0800

    Add tests for Feb 28 & Day of Week

--- function_test.go
+++ function_test.go
@@ -11,6 +11,7 @@ func TestCronRange_NextOccurrences(t *testing.T) {
 	crEmpty := &CronRange{}
 	crFirstDayEachMonth, _ := New("0 0 1 * *", "", 1440)
 	crFirstHourFeb29, _ := New("0 0 29 2 *", "", 60)
+	crFirstHourFeb28OrSun, _ := New("0 0 28 2 0", "", 60)
 	type args struct {
 		t     time.Time
 		count int
@@ -71,6 +72,19 @@ func TestCronRange_NextOccurrences(t *testing.T) {
 				{time.Date(2012, 2, 29, 0, 0, 0, 0, time.Local), time.Date(2012, 2, 29, 1, 0, 0, 0, time.Local)},
 				{time.Date(2016, 2, 29, 0, 0, 0, 0, time.Local), time.Date(2016, 2, 29, 1, 0, 0, 0, time.Local)},
 				{time.Date(2020, 2, 29, 0, 0, 0, 0, time.Local), time.Date(2020, 2, 29, 1, 0, 0, 0, time.Local)},
+			},
+			false,
+		},
+		{"first hour of feb 28 or sunday since 2016",
+			crFirstHourFeb28OrSun,
+			args{time.Date(2016, 1, 1, 0, 0, 0, 0, time.Local), 6},
+			[]TimeRange{
+				{time.Date(2016, 2, 7, 0, 0, 0, 0, time.Local), time.Date(2016, 2, 7, 1, 0, 0, 0, time.Local)},
+				{time.Date(2016, 2, 14, 0, 0, 0, 0, time.Local), time.Date(2016, 2, 14, 1, 0, 0, 0, time.Local)},
+				{time.Date(2016, 2, 21, 0, 0, 0, 0, time.Local), time.Date(2016, 2, 21, 1, 0, 0, 0, time.Local)},
+				{time.Date(2016, 2, 28, 0, 0, 0, 0, time.Local), time.Date(2016, 2, 28, 1, 0, 0, 0, time.Local)},
+				{time.Date(2017, 2, 5, 0, 0, 0, 0, time.Local), time.Date(2017, 2, 5, 1, 0, 0, 0, time.Local)},
+				{time.Date(2017, 2, 12, 0, 0, 0, 0, time.Local), time.Date(2017, 2, 12, 1, 0, 0, 0, time.Local)},
 			},
 			false,
 		},
