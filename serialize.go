4f90f99f1a421e1cb5331387b11c81e795982e7f
Angel Analoosen <an9an63@gmail.com>
2019-11-11 14:16:48 +0800

    Implement bare-metal ParseString and UnmarshalJSON

--- serialize.go
+++ serialize.go
@@ -2,7 +2,9 @@ package cronrange
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -20,6 +22,7 @@ func (cr CronRange) String() string {
 }
 
 func (tr TimeRange) String() string {
+	// TODO: better format with TZ and UTC
 	return fmt.Sprintf("[%v, %v]", tr.Start, tr.End)
 }
 
@@ -32,12 +35,61 @@ func (cr *CronRange) MarshalJSON() ([]byte, error) {
 	return json.Marshal(expr)
 }
 
-func (cr *CronRange) UnmarshalJSON(b []byte) error {
-	content := string(b)
-	fmt.Println("UM", content)
-	return nil
+var (
+	errEmptyString        = errors.New("got empty string")
+	errJsonNoQuotationFix = errors.New(`json string doesn't start or end with '"'`)
+	strDoubleQuotation    = `"`
+	strSemicolon          = `;`
+	strMarkDuration       = `DR=`
+	strMarkTimeZone       = `TZ=`
+)
+
+func (cr *CronRange) UnmarshalJSON(b []byte) (err error) {
+	if b == nil || len(b) == 0 {
+		return errEmptyString
+	}
+	raw := string(b)
+	if !(strings.HasPrefix(raw, strDoubleQuotation) && strings.HasSuffix(raw, strDoubleQuotation)) {
+		return errJsonNoQuotationFix
+	}
+
+	var newCr *CronRange
+	if newCr, err = ParseString(raw[1 : len(raw)-1]); err == nil {
+		*cr = *newCr
+	}
+	return
 }
 
-func ParseString(s string) (cr *CronRange, err error)  {
+func ParseString(s string) (cr *CronRange, err error) {
+	if len(s) == 0 {
+		err = errEmptyString
+		return
+	}
+
+	parts := strings.Split(s, strSemicolon)
+	var cronExpr, timeZone string
+	var durationMin uint64
+	idxExpr := len(parts) - 1
+
+	for idx, part := range parts {
+		part = strings.TrimSpace(part)
+
+		// cron expression must be the last part
+		if idx == idxExpr {
+			cronExpr = part
+		} else if strings.HasPrefix(part, strMarkDuration) {
+			durationStr := part[len(strMarkTimeZone):]
+			if durationMin, err = strconv.ParseUint(durationStr, 10, 64); err != nil {
+				return
+			}
+		} else if strings.HasPrefix(part, strMarkTimeZone) {
+			timeZone = part[len(strMarkDuration):]
+		} else {
+			err = errors.New(`json string has unknown part: ` + part)
+			return
+		}
+	}
+
+	cr, err = New(cronExpr, timeZone, durationMin)
 	return
 }
