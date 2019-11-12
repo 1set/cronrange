d0dfd354d3f4ea8fa84bafca16139c7a9270c9a5
Angel Analoosen <an9an63@gmail.com>
2019-11-13 07:31:31 +0800

    Fix lint

--- example_test.go
+++ example_test.go
@@ -10,12 +10,12 @@ import (
 // This example shows greeting according to your local box time.
 func Example() {
 	crGreetings := make(map[*cronrange.CronRange]string)
-	crExprGreetings := map[string]string {
-		"DR=360; 0 6 * * *": "Good morning!",
+	crExprGreetings := map[string]string{
+		"DR=360; 0 6 * * *":  "Good morning!",
 		"DR=360; 0 12 * * *": "Good afternoon!",
 		"DR=240; 0 18 * * *": "Good evening!",
 		"DR=180; 0 22 * * *": "Good night!",
-		"DR=300; 0 1 * * *": "ZzzZZzzzZZZz...",
+		"DR=300; 0 1 * * *":  "ZzzZZzzzZZZz...",
 	}
 
 	for crExpr, greeting := range crExprGreetings {
